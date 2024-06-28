3c251aea8f1f37b1b468ce8c5ec9e77f8962f4d6
rinterron <144239782+ssmitee@users.noreply.github.com>
2024-06-28 05:19:30 +0200

    Add files via upload

---
+++ ProductStackApp/ProductStackApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductStackApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ProductStack productStack = new ProductStack();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик для добавления товара в стек
+        private void AddProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            var productName = ProductNameTextBox.Text;
+            var productPrice = decimal.Parse(ProductPriceTextBox.Text);
+            var product = new Product(productName, productPrice);
+
+            productStack.Push(product);
+            MessageBox.Show("Товар добавлен в стек");
+        }
+
+        // Обработчик для отображения товаров и вычисления средней цены
+        private void DisplayProductsButton_Click(object sender, RoutedEventArgs e)
+        {
+            productStack.DisplayAll();
+            var averagePrice = productStack.CalculateAveragePrice();
+            MessageBox.Show($"Средняя цена товаров: {averagePrice}");
+        }
+    }
+}
---
+++ ProductStackApp/ProductStackApp/ProductStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProductStackApp
+{
+    public class ProductStack
+    {
+        private Stack<Product> stack = new Stack<Product>();
+
+        public void Push(Product product)
+        {
+            stack.Push(product);
+        }
+        public void DisplayAll()
+        {
+            foreach (var product in stack)
+            {
+                MessageBox.Show($"Наименование: {product.Name}, Цена: {product.Price}");
+            }
+        }
+
+        public decimal CalculateAveragePrice()
+        {
+            if (stack.Count == 0)
+                return 0;
+
+            return stack.Average(product => product.Price);
+        }
+    }
+
+}
